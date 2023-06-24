b103b0b054ca769949ded0cbae68f8a9286042b2
Matheus-fs-campos <75811967+Matheusfscampos@users.noreply.github.com>
2023-06-23 23:35:24 -0300

    migrando para o typescript

--- core/crud.ts
+++ core/crud.ts
@@ -1,12 +1,13 @@
-const fs = require("fs"); //lib do node
+import fs from "fs"; //ES6
+//const fs = require("fs"); //lib do node
 const DB_FILE_PATH = "./core/db";
 
 console.log("[crud]");
 
-function create(content){
+function create(content: string){
     //precisa salvar o content no sistema
     fs.writeFileSync(DB_FILE_PATH, content);
     return content;
 }
 
-console.log(create("Hello world!!"));
+console.log(create("Hello WORLD!!!!!!"));
