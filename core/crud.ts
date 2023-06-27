5221a2e4a7f3847f323be68784fe11c16d9bb62f
Matheus-fs-campos <75811967+Matheusfscampos@users.noreply.github.com>
2023-06-26 21:02:52 -0300

    Construindo o READ

--- core/crud.ts
+++ core/crud.ts
@@ -4,10 +4,49 @@ const DB_FILE_PATH = "./core/db";
 
 console.log("[crud]");
 
+interface Todo{
+    date: String;
+    content: string;
+    done: boolean;
+}
+
 function create(content: string){
+    const todo: Todo ={
+        date: new Date().toISOString(),
+        content: content,
+        done: false,
+    };
+
+    const todos: Array <Todo> =[
+        ...read(), // read trabalha com array, então espalhamos o array
+        todo,
+    ];
+
     //precisa salvar o content no sistema
-    fs.writeFileSync(DB_FILE_PATH, content);
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+        dogs: [],
+    }, null, 2));
     return content;
 }
 
-console.log(create("Hello WORLD!!!!!!"));
+function read(): Array<Todo>{
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}"); // se vazio o arquivo retorna o "{}"
+    if(!db.todos){ //fail fast validation: Não tem nada preenchido? Ja para!!
+        return [];
+    }
+    return db.todos;
+}
+
+function CLEAR_DB(){
+    fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+//[SIMULAÇÃO]
+
+CLEAR_DB();
+
+create("primeira TODO");
+create("segunda TODO");
+console.log(read());
