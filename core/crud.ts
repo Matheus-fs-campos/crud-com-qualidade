3625adb2d95f8e1f30c7ff7d66c36a7482cfbfb7
matheus-fs-campos <75811967+Matheusfscampos@users.noreply.github.com>
2023-07-11 22:19:31 -0300

    Update - object assign e lib uuid

--- core/crud.ts
+++ core/crud.ts
@@ -1,17 +1,20 @@
 import fs from "fs"; //ES6
+import { v4 as uuid } from 'uuid';
 //const fs = require("fs"); //lib do node
 const DB_FILE_PATH = "./core/db";
 
 console.log("[crud]");
 
 interface Todo{
+    id: string;
     date: String;
     content: string;
     done: boolean;
 }
 
-function create(content: string){
+function create(content: string): Todo{
     const todo: Todo ={
+        id: uuid(),
         date: new Date().toISOString(),
         content: content,
         done: false,
@@ -26,8 +29,8 @@ function create(content: string){
     fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
         todos,
         dogs: [],
-    }, null, 2));
-    return content;
+    }, null, 2)); //replace null e espaçamento 2
+    return todo;
 }
 
 function read(): Array<Todo>{
@@ -39,6 +42,32 @@ function read(): Array<Todo>{
     return db.todos;
 }
 
+function update(id: string, partialTodo: Partial<Todo>){
+    let updatadedTodo;
+    const todos = read();
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.id === id;
+        if(isToUpdate){
+            updatadedTodo = Object.assign(currentTodo, partialTodo); // qqr valor do partialTodo altera o currentTodo
+        }
+    });
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos
+    }, null, 2));
+
+    if(!updatadedTodo){
+        throw new Error("Please, provide another ID")
+    }
+    return updatadedTodo;
+}
+
+function updateContentById(id: string, content: string): Todo{
+    return update(id, {
+        content,
+    });
+}
+
 function CLEAR_DB(){
     fs.writeFileSync(DB_FILE_PATH, "");
 }
@@ -48,5 +77,14 @@ function CLEAR_DB(){
 CLEAR_DB();
 
 create("primeira TODO");
-create("segunda TODO");
+create("segunda TODO1");
+const terceiraToDo = create("terceira TODO1");
+
+//update(terceiraToDo.id, {
+//    content: "Atualizada",
+//    done: true
+//});// update(dequem, oque)
+
+updateContentById(terceiraToDo.id, "Atualizada");
 console.log(read());
+
